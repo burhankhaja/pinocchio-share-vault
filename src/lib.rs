98c2e6c82b0beefc2ff3b9ae96ff33ea2ec7448f
burhankhaja <karhazemail@gmail.com>
2025-08-22 17:29:29 +0000

    Handle entrypoint logic for initialize instruction

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,31 @@
 #![no_std]
-use pinocchio::{entrypoint, nostd_panic_handler, ProgramResult, pubkey::Pubkey, account_info::AccountInfo};
-// pub mod instructions;
+use pinocchio::{
+    account_info::AccountInfo, entrypoint, nostd_panic_handler, program_error::ProgramError,
+    pubkey::Pubkey, ProgramResult,
+};
+
+use crate::instructions::Initialize;
+pub mod instructions;
 
 nostd_panic_handler!();
 entrypoint!(process_instruction);
 
+// 22222222222222222222222222222222222222222222
+pub const ID: Pubkey = [
+    0x0f, 0x1e, 0x6b, 0x14, 0x21, 0xc0, 0x4a, 0x07, 0x04, 0x31, 0x26, 0x5c, 0x19, 0xc5, 0xbb, 0xee,
+    0x19, 0x92, 0xba, 0xe8, 0xaf, 0xd1, 0xcd, 0x07, 0x8e, 0xf8, 0xaf, 0x70, 0x47, 0xdc, 0x11, 0xf7,
+];
+
 pub fn process_instruction(
     _program_id: &Pubkey,
     _accounts: &[AccountInfo],
-    _data: &[u8],
+    _instruction_data: &[u8],
 ) -> ProgramResult {
+    match _instruction_data.split_first() {
+        Some((Initialize::DISCRIMINATOR, _data)) => {
+            Initialize::try_from(_accounts)?.process(_program_id)?
+        }
+        _ => Err(ProgramError::InvalidInstructionData)?,
+    }
     Ok(())
 }
