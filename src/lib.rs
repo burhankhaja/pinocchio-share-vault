2f0850a7d94c23a62aa93d350444e1f8f0c279bb
burhankhaja <karhazemail@gmail.com>
2025-08-23 16:09:27 +0000

    Handle Redeem entrypoint

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,7 @@ use pinocchio::{
 };
 
 pub mod instructions;
-use crate::instructions::{Deposit, Initialize};
+use crate::instructions::{Deposit, Initialize, Redeem};
 
 nostd_panic_handler!();
 entrypoint!(process_instruction);
@@ -25,6 +25,7 @@ pub fn process_instruction(
     match _instruction_data.split_first() {
         Some((Initialize::DISCRIMINATOR, _)) => Initialize::try_from(_accounts)?.process(_program_id)?,
         Some((Deposit::DISCRIMINATOR, data)) => Deposit::try_from((_accounts, data))?.process()?,
+        Some((Redeem::DISCRIMINATOR, data)) => Redeem::try_from((_accounts, data))?.process()?,
         _ => Err(ProgramError::InvalidInstructionData)?,
     }
     Ok(())
